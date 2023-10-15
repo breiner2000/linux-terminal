7db9886747fa7ab727a7b87f2719847583d21ce7
breiner2000 <breinergcp@estudiantec.cr>
2023-10-15 13:05:02 -0600

    fix user for admin user script

--- scripts/config_admin_user.sh
+++ scripts/config_admin_user.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+username=$(whoami)
+
 # Paso 1: Crear el directorio si no existe
 sudo mkdir -p /etc/sudoers.d/
 
 # Paso 2: Crear o editar el archivo con el contenido deseado
-echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$USER
+echo "$username ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$username
+
 
 # Verificar si la configuración se aplicó correctamente
 if [ $? -eq 0 ]; then
