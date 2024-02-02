9229f62baad1bfb417ee93dad33519e6fec12b84
breiner2000 <breinergcp@estudiantec.cr>
2024-02-01 22:17:41 -0600

    print function

--- initial.sh
+++ initial.sh
@@ -24,59 +24,21 @@ starship_res=$?
 kitty_res=$?
 
 
-# Verificar el código de salida y actuar en consecuencia
-if [ "$font_res" -eq 0 ]; then
-    echo -e "\e[1;32m Fira Code Nerd Font ha sido instalada.
-    \e[0m"
-else
-    echo -e "\e[1;31m Error al instalar Fira Code Nerd Font.
-    \e[0m"
-fi
+print_status() {
+    if [ "$1" -eq 0 ]; then
+        echo -e "\e[1;32m $2
+        \e[0m"
+    else
+        echo -e "\e[1;31m $3
+        \e[0m"
+    fi
+}
 
-if [ "$sudo_res" -eq 0 ]; then
-    echo -e "\e[1;32m El usuario $USER ha sido configurado como administrador.
-    \e[0m"
-else
-    echo -e "\e[1;31m Error al configurar el usuario $USER como administrador.
-    \e[0m"
-fi
-
-if [ "$zsh_res" -eq 0 ]; then
-    echo -e "\e[1;32m Los paquetes Zsh, curl, wget most y exa han sido instalados.
-    \e[0m"
-else
-    echo -e "\e[1;31m Error al instalar Zsh, curl, wget most o exa.
-    \e[0m"
-fi
-
-if [ "$ohmyzsh_res" -eq 0 ]; then
-    echo -e "\e[1;32m Oh My Zsh ha sido instalado.
-    \e[0m"
-else
-    echo -e "\e[1;31m Error al instalar Oh My Zsh.
-    \e[0m"
-fi
-
-if [ "$zsh_autosuggestions_res" -eq 0 ]; then
-    echo -e "\e[1;32m Zsh Autosuggestions ha sido instalado.
-    \e[0m"
-else
-    echo -e "\e[1;31m Error al instalar Zsh Autosuggestions.
-    \e[0m"
-fi
-
-if [ "$starship_res" -eq 0 ]; then
-    echo -e "\e[1;32m Starship ha sido instalado.
-    \e[0m"
-else
-    echo -e "\e[1;31m Error al instalar Starship.
-    \e[0m"
-fi
-
-if [ "$kitty_res" -eq 0 ]; then
-    echo -e "\e[1;32m Kitty ha sido instalado.
-    \e[0m"
-else
-    echo -e "\e[1;31m Error al instalar Kitty.
-    \e[0m"
-fi
+# Imprimir el estado de la instalación
+print_status $font_res "Fira Code Nerd Font ha sido instalada." "Error al instalar Fira Code Nerd Font."
+print_status $sudo_res "El usuario $USER ha sido configurado como administrador." "Error al configurar el usuario $USER como administrador."
+print_status $zsh_res "Los paquetes Zsh, curl, wget most y exa han sido instalados." "Error al instalar Zsh, curl, wget most o exa."
+print_status $ohmyzsh_res "Oh My Zsh ha sido instalado." "Error al instalar Oh My Zsh."
+print_status $zsh_autosuggestions_res "Zsh Autosuggestions ha sido instalado." "Error al instalar Zsh Autosuggestions."
+print_status $starship_res "Starship ha sido instalado." "Error al instalar Starship."
+print_status $kitty_res "Kitty ha sido instalado." "Error al instalar Kitty."
